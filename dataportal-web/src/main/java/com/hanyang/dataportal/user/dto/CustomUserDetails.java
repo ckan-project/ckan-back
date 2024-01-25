50c8c9e81f6ede064a72d5651eb73fda207ea41f
minSsan <msp1125@hanyang.ac.kr>
2024-01-26 00:02:01 +0900

    feat: main 브랜치 병합 - user auth 관련 오류 해결

--- dataportal-web/src/main/java/com/hanyang/dataportal/user/dto/CustomUserDetails.java
+++ dataportal-web/src/main/java/com/hanyang/dataportal/user/dto/CustomUserDetails.java
@@ -1,66 +1,44 @@
-package com.hanyang.dataportal.user.dto;
-
-import com.hanyang.dataportal.user.domain.User;
-import lombok.Getter;
-import org.springframework.security.core.GrantedAuthority;
-import org.springframework.security.core.authority.SimpleGrantedAuthority;
-import org.springframework.security.core.userdetails.UserDetails;
-
-import java.util.ArrayList;
-import java.util.Collection;
-import java.util.List;
-
-@Getter
-public class CustomUserDetails implements UserDetails {
-
-    private final String email;
-    private final String password;
-    private final String name;
-    private final Collection<? extends GrantedAuthority> authorities;
-    private final boolean isActive;
-
-    public CustomUserDetails(User user) {
-        this.email = user.getEmail();
-        this.password = user.getPassword();
-        this.name = user.getName();
-        List<GrantedAuthority> authorities = new ArrayList<>();
-        authorities.add(new SimpleGrantedAuthority(user.getRole().name()));
-        this.authorities = authorities;
-        this.isActive = user.isActive();
-    }
-
-    @Override
-    public Collection<? extends GrantedAuthority> getAuthorities() {
-        return authorities;
-    }
-
-    @Override
-    public String getPassword() {
-        return this.password;
-    }
-
-    @Override
-    public String getUsername() {
-        return this.email;
-    }
-
-    @Override
-    public boolean isAccountNonExpired() {
-        return true;
-    }
-
-    @Override
-    public boolean isAccountNonLocked() {
-        return true;
-    }
-
-    @Override
-    public boolean isCredentialsNonExpired() {
-        return true;
-    }
-
-    @Override
-    public boolean isEnabled() {
-        return this.isActive;
-    }
-}
+//package com.hanyang.dataportal.user.dto;
+//
+//import com.hanyang.dataportal.user.domain.User;
+//import lombok.Getter;
+//import org.springframework.security.core.GrantedAuthority;
+//
+//import java.util.Collection;
+//
+//@Getter
+//public class CustomUserDetails extends org.springframework.security.core.userdetails.User {
+//
+//    private final String name;
+//    private final boolean isActive;
+//
+//    public CustomUserDetails(String username, String password, Collection<? extends GrantedAuthority> authorities,User user) {
+//        super(username, password, authorities);
+//        this.name = user.getName();
+//        this.isActive = user.isActive();
+//    }
+//
+//    @Override
+//    public boolean isAccountNonExpired() {
+//        return true;
+//    }
+//
+//    @Override
+//    public boolean isAccountNonLocked() {
+//        return true;
+//    }
+//
+//    @Override
+//    public boolean isCredentialsNonExpired() {
+//        return true;
+//    }
+//
+//    @Override
+//    public boolean isEnabled() {
+//        return this.isActive;
+//    }
+//
+//    public String getName() {
+//        return name;
+//    }
+//}
