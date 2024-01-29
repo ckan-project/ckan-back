bb547c17bb105e2c505b37435f7c4409eb370621
minSsan <msp1125@hanyang.ac.kr>
2024-01-29 16:13:46 +0900

    chore: swagger 예제 추가

--- dataportal-web/src/main/java/com/hanyang/dataportal/user/dto/req/ReqScrapDto.java
+++ dataportal-web/src/main/java/com/hanyang/dataportal/user/dto/req/ReqScrapDto.java
@@ -1,8 +1,10 @@
 package com.hanyang.dataportal.user.dto.req;
 
+import io.swagger.v3.oas.annotations.media.Schema;
 import lombok.Data;
 
 @Data
 public class ReqScrapDto {
+    @Schema(description = "스크랩 하는 데이터셋의 id값(숫자)", example = "1")
     private Long datasetId;
 }
