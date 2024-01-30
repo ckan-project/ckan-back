1415293e12d5e2a754648aa93304d822240688dc
minSsan <msp1125@hanyang.ac.kr>
2024-01-30 16:22:32 +0900

    feat: ScrapController - scrap service 클래스 분리 적용

--- dataportal-web/src/main/java/com/hanyang/dataportal/user/controller/ScrapController.java
+++ dataportal-web/src/main/java/com/hanyang/dataportal/user/controller/ScrapController.java
@@ -4,6 +4,7 @@ import com.hanyang.dataportal.core.dto.ApiResponse;
 import com.hanyang.dataportal.user.domain.Scrap;
 import com.hanyang.dataportal.user.dto.res.ResScrapDto;
 import com.hanyang.dataportal.user.service.ScrapService;
+import com.hanyang.dataportal.user.service.UpdateScrapService;
 import io.swagger.v3.oas.annotations.Operation;
 import io.swagger.v3.oas.annotations.tags.Tag;
 import lombok.RequiredArgsConstructor;
@@ -19,6 +20,7 @@ import java.util.List;
 @Tag(name = "스크랩 API")
 public class ScrapController {
     private final ScrapService scrapService;
+    private final UpdateScrapService updateScrapService;
 
     /**
      * 유저의 모든 스크랩 내역을 가져오는 메서드
@@ -57,7 +59,7 @@ public class ScrapController {
     @Operation(summary = "로그인 유저의 새로운 스크랩 생성")
     @PostMapping("/api/dataset/{datasetId}/scrap")
     public ResponseEntity<ApiResponse<?>> createScrap(@AuthenticationPrincipal UserDetails userDetails, @PathVariable Long datasetId) {
-        Scrap scrap = scrapService.createScrap(userDetails, datasetId);
+        Scrap scrap = updateScrapService.createScrap(userDetails, datasetId);
         ResScrapDto resScrapDto = new ResScrapDto(scrap);
         return ResponseEntity.ok(ApiResponse.ok(resScrapDto));
     }
@@ -71,7 +73,7 @@ public class ScrapController {
     @Operation(summary = "로그인 유저의 특정 스크랩 내역 삭제")
     @DeleteMapping("/api/dataset/{datasetId}/scrap")
     public ResponseEntity<ApiResponse<?>> deleteScrap(@AuthenticationPrincipal UserDetails userDetails, @PathVariable Long datasetId) {
-        Scrap scrap = scrapService.removeScrap(userDetails, datasetId);
+        Scrap scrap = updateScrapService.removeScrap(userDetails, datasetId);
         return ResponseEntity.ok(ApiResponse.ok(new ResScrapDto(scrap)));
     }
 }
