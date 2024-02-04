74e2da23743451b97d5388909228c9d35726e021
minSsan <msp1125@hanyang.ac.kr>
2024-02-04 12:24:26 +0900

    test: ScrapService - checkDuplicateByDatasetAndUser 메서드 테스트 코드 추가

--- dataportal-web/src/test/java/com/hanyang/dataportal/user/service/ScrapServiceTest.java
+++ dataportal-web/src/test/java/com/hanyang/dataportal/user/service/ScrapServiceTest.java
@@ -1,15 +1,22 @@
 package com.hanyang.dataportal.user.service;
 
-import com.hanyang.dataportal.dataset.repository.DatasetRepository;
+import com.hanyang.dataportal.core.dto.ResponseMessage;
+import com.hanyang.dataportal.core.exception.ResourceExist;
+import com.hanyang.dataportal.dataset.domain.Dataset;
+import com.hanyang.dataportal.user.domain.Scrap;
+import com.hanyang.dataportal.user.domain.User;
 import com.hanyang.dataportal.user.repository.ScrapRepository;
-import com.hanyang.dataportal.user.repository.UserRepository;
+import org.assertj.core.api.Assertions;
 import org.junit.jupiter.api.*;
 import org.junit.jupiter.api.extension.ExtendWith;
 import org.mockito.InjectMocks;
 import org.mockito.Mock;
 import org.mockito.junit.jupiter.MockitoExtension;
 
-import static org.mockito.ArgumentMatchers.any;
+import java.util.Optional;
+
+import static org.junit.jupiter.api.Assertions.assertThrows;
+import static org.mockito.Mockito.when;
 
 @ExtendWith(MockitoExtension.class)
 class ScrapServiceTest {
@@ -17,31 +24,46 @@ class ScrapServiceTest {
     private ScrapService scrapService;
 
     @Mock
-    UserRepository userRepository;
-    @Mock
     ScrapRepository scrapRepository;
-    @Mock
-    DatasetRepository datasetRepository;
-    @Mock
-    CustomUserDetailsService customUserDetailsService;
+
+    private static Long datasetId;
+    private static String userEmail;
+
+    @BeforeAll
+    static void setup() {
+        datasetId = 1L;
+        userEmail = "test";
+    }
 
     @Nested
-    @DisplayName("스크랩 생성 테스트")
-    class CreateScrap { }
+    @DisplayName("예외 케이스")
+    class FailCase {
+        @Test
+        @DisplayName("중복 스크랩 존재 여부 확인하기")
+        void checkDuplicateByDatasetAndUser() {
+            // given
+            Dataset dataset = Dataset.builder()
+                    .datasetId(datasetId)
+                    .build();
+            User user = User.builder()
+                    .email(userEmail)
+                    .build();
+            Scrap scrap = Scrap.builder()
+                    .user(user)
+                    .dataset(dataset)
+                    .build();
 
-    @Test
-    void findAllByEmail() {
-    }
+            when(scrapRepository.findByDatasetAndUser(dataset, user))
+                    .thenReturn(Optional.of(scrap));
 
-    @Test
-    void findByScrapId() {
-    }
+            // when
+            Exception exception = assertThrows(ResourceExist.class, () -> {
+                scrapService.checkDuplicateByDatasetAndUser(dataset, user);
+            });
 
-    @Test
-    void createScrap() {
-    }
-
-    @Test
-    void removeScrap() {
+            // then
+            Assertions.assertThat(exception.getMessage())
+                    .isEqualTo(ResponseMessage.DUPLICATE_SCRAP);
+        }
     }
 }
