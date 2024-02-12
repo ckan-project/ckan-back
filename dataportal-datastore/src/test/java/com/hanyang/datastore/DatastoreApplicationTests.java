ab0b7a27fc633793cc4d12bddb2636b04354e79e
mintaek22 <biba99@naver.com>
2024-02-12 13:52:55 +0900

    chore:grdle 세팅 수정

--- dataportal-datastore/src/test/java/com/hanyang/datastore/DatastoreApplicationTests.java
+++ dataportal-datastore/src/test/java/com/hanyang/datastore/DatastoreApplicationTests.java
@@ -1,13 +1,7 @@
 package com.hanyang.datastore;
 
-import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
 @SpringBootTest
 class DatastoreApplicationTests {
-
-	@Test
-	void contextLoads() {
-	}
-
 }
