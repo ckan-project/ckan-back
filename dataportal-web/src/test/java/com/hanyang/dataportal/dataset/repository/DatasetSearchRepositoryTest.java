f79c5a5714794d895b33b1d7e9d752f8f92f1569
mintaek22 <biba99@naver.com>
2024-02-19 14:26:10 +0900

    test:데이터셋 리스트 조회 테스트 코드 추가

---
+++ dataportal-web/src/test/java/com/hanyang/dataportal/dataset/repository/DatasetSearchRepositoryTest.java
@@ -0,0 +1,126 @@
+package com.hanyang.dataportal.dataset.repository;
+
+import com.hanyang.dataportal.TestQueryDSLConfig;
+import com.hanyang.dataportal.dataset.domain.Dataset;
+import com.hanyang.dataportal.dataset.domain.Organization;
+import com.hanyang.dataportal.dataset.domain.Theme;
+import com.hanyang.dataportal.dataset.dto.DatasetSearchCond;
+import com.hanyang.dataportal.dataset.utill.DatasetSort;
+import org.assertj.core.api.Assertions;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.jdbc.AutoConfigureTestDatabase;
+import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest;
+import org.springframework.context.annotation.Import;
+import org.springframework.data.domain.Page;
+import org.springframework.test.context.ActiveProfiles;
+
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.List;
+
+@ActiveProfiles("test")
+@DataJpaTest
+@Import(TestQueryDSLConfig.class)
+@AutoConfigureTestDatabase(replace = AutoConfigureTestDatabase.Replace.NONE)
+class DatasetSearchRepositoryTest {
+    @Autowired
+    private DatasetRepository datasetRepository;
+    @Autowired
+    private DatasetSearchRepository datasetSearchRepository;
+    @BeforeEach
+    void setUp() {
+        List<Dataset> datasetList = new ArrayList<>();
+        for (int i = 0; i < 21; i++) {
+            Dataset.DatasetBuilder da = Dataset.builder().
+                    title("20" + i + "년도").
+                    view(100-i);
+            if(i<13){
+                da.organization(Organization.소프트융합대학);
+            }
+            else{
+                da.organization(Organization.공과대학);
+            }
+
+            Dataset dataset = da.build();
+            dataset.addTheme(Theme.입학);
+            if(i<10){
+                dataset.addTheme(Theme.장학);
+            }
+            else{
+                dataset.addTheme(Theme.재정);
+            }
+
+            datasetList.add(dataset);
+        }
+        datasetRepository.saveAll(datasetList);
+    }
+
+    @DisplayName("데이터셋 KeyWord 테스트")
+    @Test
+    void searchDatasetListKeyWord() {
+        //given
+        DatasetSearchCond datasetSearchCond = new DatasetSearchCond();
+        datasetSearchCond.setPage(0);
+        datasetSearchCond.setKeyword("2011");
+        datasetSearchCond.setSort(DatasetSort.최신);
+        //when
+        Page<Dataset> datasets = datasetSearchRepository.searchDatasetList(datasetSearchCond);
+
+        //then
+        Assertions.assertThat(datasets.getTotalElements()).isEqualTo(1);
+        Assertions.assertThat(datasets.getTotalPages()).isEqualTo(1);
+    }
+
+    @DisplayName("데이터셋 Organization 테스트")
+    @Test
+    void searchDatasetListOrganization() {
+        //given
+        DatasetSearchCond datasetSearchCond = new DatasetSearchCond();
+        datasetSearchCond.setPage(0);
+        datasetSearchCond.setOrganization(Organization.소프트융합대학);
+        datasetSearchCond.setSort(DatasetSort.최신);
+        //when
+        Page<Dataset> datasets = datasetSearchRepository.searchDatasetList(datasetSearchCond);
+
+        //then
+        Assertions.assertThat(datasets.getTotalElements()).isEqualTo(13);
+        Assertions.assertThat(datasets.getTotalPages()).isEqualTo(2);
+    }
+
+    @DisplayName("데이터셋 Theme 테스트")
+    @Test
+    void searchDatasetListTheme() {
+        //given
+        DatasetSearchCond datasetSearchCond = new DatasetSearchCond();
+        datasetSearchCond.setPage(0);
+        datasetSearchCond.setTheme(Collections.singletonList(Theme.입학));
+        datasetSearchCond.setSort(DatasetSort.최신);
+
+        //when
+        Page<Dataset> datasets = datasetSearchRepository.searchDatasetList(datasetSearchCond);
+
+        //then
+        Assertions.assertThat(datasets.getTotalElements()).isEqualTo(21);
+        Assertions.assertThat(datasets.getTotalPages()).isEqualTo(3);
+    }
+
+    @DisplayName("데이터셋 Sort 조회 테스트")
+    @Test
+    void searchDatasetListSort() {
+        //given
+        DatasetSearchCond datasetSearchCond = new DatasetSearchCond();
+        datasetSearchCond.setPage(0);
+        datasetSearchCond.setSort(DatasetSort.조회);
+
+        //when
+        Page<Dataset> datasets = datasetSearchRepository.searchDatasetList(datasetSearchCond);
+
+        //then
+        Assertions.assertThat(datasets.getTotalElements()).isEqualTo(21);
+        Assertions.assertThat(datasets.getTotalPages()).isEqualTo(3);
+        Assertions.assertThat(datasets.getContent().get(0).getView()).isGreaterThan(datasets.getContent().get(1).getView());
+    }
+}
