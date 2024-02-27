7d969151702f6699dfec60583f59cea31c255cce
minSsan <msp1125@hanyang.ac.kr>
2024-02-28 00:13:11 +0900

    feat: 카카오 로그인 완료

--- dataportal-web/src/main/java/com/hanyang/dataportal/user/controller/KakaoController.java
+++ dataportal-web/src/main/java/com/hanyang/dataportal/user/controller/KakaoController.java
@@ -1,8 +1,16 @@
 package com.hanyang.dataportal.user.controller;
 
+import com.hanyang.dataportal.core.response.ApiResponse;
+import com.hanyang.dataportal.user.dto.req.ReqLoginDto;
+import com.hanyang.dataportal.user.dto.req.ReqSignupDto;
 import com.hanyang.dataportal.user.service.KakaoLoginService;
+import com.hanyang.dataportal.user.service.UserLoginService;
+import com.hanyang.dataportal.user.service.UserService;
+import com.hanyang.dataportal.user.service.UserSignupService;
 import io.swagger.v3.oas.annotations.tags.Tag;
 import lombok.RequiredArgsConstructor;
+import org.json.simple.parser.ParseException;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
@@ -14,12 +22,33 @@ import java.util.Map;
 @RequiredArgsConstructor
 public class KakaoController {
     private final KakaoLoginService kakaoLoginService;
+    private final UserService userService;
+    private final UserLoginService userLoginService;
+    private final UserSignupService userSignupService;
+
     @GetMapping("login/oauth2/kakao")
-    public void kakaoLogin(@RequestParam String code) {
-        String accessToken = kakaoLoginService.getAccessToken(code);
-        Map<String, Object> userInfo = kakaoLoginService.getUserInfo(accessToken);
-        userInfo.forEach((key, value) -> {
-            System.out.println("key: " + key + ", value: " + value);
-        });
+    public ResponseEntity<ApiResponse<?>> kakaoLogin(@RequestParam String code) {
+        try {
+            String accessToken = kakaoLoginService.getAccessToken(code);
+            Map<String, String> userInfo = kakaoLoginService.getUserInfo(accessToken);
+            String userId = userInfo.get("userId");
+            // 이미 카카오 연동이 완료된 회원인 경우 -> 로그인 처리
+            if (userService.isExistByEmail(userId)) {
+                ReqLoginDto reqLoginDto = new ReqLoginDto();
+                reqLoginDto.setEmail(userId);
+                reqLoginDto.setPassword(userId);
+                return ResponseEntity.ok(ApiResponse.ok(userLoginService.login(reqLoginDto)));
+            }
+            // 카카오 정보로 회원가입 진행
+            String name = userInfo.get("name");
+            ReqSignupDto reqSignupDto = new ReqSignupDto();
+            reqSignupDto.setEmail(userId);
+            reqSignupDto.setPassword(userId);
+            reqSignupDto.setName(name);
+            return ResponseEntity.ok(ApiResponse.ok(userSignupService.signUp(reqSignupDto)));
+        } catch (ParseException e) {
+            // 카카오 API와 json 파싱 값이 일치하지 않는 경우 -> 5xx error
+            return ResponseEntity.internalServerError().build();
+        }
     }
 }
--- dataportal-web/src/main/java/com/hanyang/dataportal/user/service/KakaoLoginService.java
+++ dataportal-web/src/main/java/com/hanyang/dataportal/user/service/KakaoLoginService.java
@@ -1,6 +1,6 @@
 package com.hanyang.dataportal.user.service;
 
-import com.hanyang.dataportal.user.dto.res.ResKakaoAccessTokenDto;
+import lombok.RequiredArgsConstructor;
 import org.json.simple.JSONObject;
 import org.json.simple.parser.JSONParser;
 import org.json.simple.parser.ParseException;
@@ -9,6 +9,7 @@ import org.springframework.http.HttpHeaders;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.MediaType;
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
 import org.springframework.util.LinkedMultiValueMap;
 import org.springframework.util.MultiValueMap;
 import org.springframework.web.reactive.function.BodyInserters;
@@ -18,6 +19,8 @@ import java.util.HashMap;
 import java.util.Map;
 
 @Service
+@Transactional
+@RequiredArgsConstructor
 public class KakaoLoginService {
     @Value("${kakao.rest_api_key}")
     private String client_id;
@@ -31,12 +34,14 @@ public class KakaoLoginService {
     @Value("${kakao.userInfo_url}")
     private String userInfo_url;
 
+    private final JSONParser jsonParser = new JSONParser();
+
     /**
      * 액세스 토큰을 발급받는 메서드
      * @param code redirect로 넘겨받은 인가코드
      * @return
      */
-    public String getAccessToken(String code) {
+    public String getAccessToken(String code) throws ParseException {
         MultiValueMap<String, String> formData = new LinkedMultiValueMap<>();
         formData.add("grant_type", "authorization_code");
         formData.add("client_id", client_id);
@@ -44,7 +49,7 @@ public class KakaoLoginService {
         formData.add("code", code);
 
         WebClient webClient = WebClient.create(token_url);
-        ResKakaoAccessTokenDto response = webClient.post()
+        String response = webClient.post()
                 .header("Content-type", "application/x-www-form-urlencoded;charset=utf-8")
                 .body(BodyInserters.fromFormData(formData))
                 .accept(MediaType.APPLICATION_JSON)
@@ -53,15 +58,11 @@ public class KakaoLoginService {
                         HttpStatus.INTERNAL_SERVER_ERROR::equals,
                         clientResponse -> clientResponse.bodyToMono(String.class).map(Exception::new)
                 )
-                .bodyToMono(ResKakaoAccessTokenDto.class)
+                .bodyToMono(String.class)
                 .block();
 
-        System.out.println(response);
-        if (response == null) {
-            throw new RuntimeException(token_url + ": POST 요청 응답이 존재하지 않습니다.");
-        }
-
-        return response.getAccess_token();
+        JSONObject responseObj = (JSONObject) jsonParser.parse(response);
+        return responseObj.get("access_token").toString();
     }
 
     /**
@@ -69,39 +70,30 @@ public class KakaoLoginService {
      * @param accessToken 발급받은 액세스 토큰
      * @return
      */
-    public Map<String, Object> getUserInfo(String accessToken) {
-        Map<String, Object> result = new HashMap<>();
-        try {
-            WebClient webClient = WebClient.create(userInfo_url);
-            String response = webClient.get()
-                    .headers(httpHeaders -> {
-                        httpHeaders.add(HttpHeaders.AUTHORIZATION, String.format("Bearer %s", accessToken));
-                        httpHeaders.add(HttpHeaders.CONTENT_TYPE, "application/x-www-form-urlencoded;charset=utf-8");
-                    })
-                    .retrieve()
-                    .bodyToMono(String.class)
-                    .block();
+    public Map<String, String> getUserInfo(String accessToken) throws ParseException {
+        Map<String, String> result = new HashMap<>();
 
-            System.out.println(response);
+        WebClient webClient = WebClient.create(userInfo_url);
+        String response = webClient.get()
+                .headers(httpHeaders -> {
+                    httpHeaders.add(HttpHeaders.AUTHORIZATION, String.format("Bearer %s", accessToken));
+                    httpHeaders.add(HttpHeaders.CONTENT_TYPE, "application/x-www-form-urlencoded;charset=utf-8");
+                })
+                .retrieve()
+                .bodyToMono(String.class)
+                .block();
 
-            // parsing response to JSON
-            JSONParser parser = new JSONParser();
-            JSONObject responseObj = (JSONObject) parser.parse(response);
-            JSONObject properties = (JSONObject) responseObj.get("properties");
-            JSONObject kakao_account = (JSONObject) responseObj.get("kakao_account");
+        // parsing response to JSON
+        JSONObject responseObj = (JSONObject) jsonParser.parse(response);
+        JSONObject properties = (JSONObject) responseObj.get("properties");
 
-            // parsing required values
-            String userId = responseObj.get("id").toString();
-            String name = properties.get("nickname").toString();
-            String email = kakao_account.get("email").toString();
+        // parsing required values
+        String userId = responseObj.get("id").toString();
+        String name = properties.get("nickname").toString();
 
-            // 리턴값 구성
-            result.put("userId", userId);
-            result.put("email", email);
-            result.put("name", name);
-        } catch (ParseException e) {
-            throw new RuntimeException("프로퍼티가 존재하지 않습니다.");
-        }
+        // 리턴값 구성
+        result.put("userId", userId);
+        result.put("name", name);
 
         return result;
     }
