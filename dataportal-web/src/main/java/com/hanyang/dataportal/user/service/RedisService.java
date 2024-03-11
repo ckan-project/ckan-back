29caee28887172801fb332a665196572621174ca
minSsan <msp1125@hanyang.ac.kr>
2024-03-11 21:35:03 +0900

    refactor: RedisService setCode 메서드 오버로딩

--- dataportal-web/src/main/java/com/hanyang/dataportal/user/service/RedisService.java
+++ dataportal-web/src/main/java/com/hanyang/dataportal/user/service/RedisService.java
@@ -2,6 +2,7 @@ package com.hanyang.dataportal.user.service;
 
 import com.hanyang.dataportal.core.exception.UnAuthenticationException;
 import com.hanyang.dataportal.core.response.ResponseMessage;
+import jakarta.annotation.Resource;
 import lombok.RequiredArgsConstructor;
 import org.springframework.data.redis.core.RedisTemplate;
 import org.springframework.data.redis.core.ValueOperations;
@@ -14,10 +15,22 @@ import java.util.concurrent.TimeUnit;
 public class RedisService {
     private final RedisTemplate<String, Object> redisTemplate;
 
+    @Resource(name = "redisTemplate")
+    private ValueOperations<String, Object> valueOperations;
+
     public void setCode(String email,String code){
-        ValueOperations<String, Object> valOperations = redisTemplate.opsForValue();
         //만료기간 3분
-        valOperations.set(email,code,180, TimeUnit.SECONDS);
+        valueOperations.set(email,code,180, TimeUnit.SECONDS);
+    }
+
+    /**
+     *
+     * @param email redis key 값으로 넣을 유저 이메일
+     * @param code redis value 값으로 넣을 값
+     * @param expire TTL (초 단위)
+     */
+    public void setCode(String email, String code, Long expire) {
+        valueOperations.set(email, code, expire, TimeUnit.SECONDS);
     }
 
     public String getCode(String email){
