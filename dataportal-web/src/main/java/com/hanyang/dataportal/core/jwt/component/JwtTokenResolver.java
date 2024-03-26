62680b51134149baf379151c6bead65fad5bf9b9
minSsan <msp1125@hanyang.ac.kr>
2024-03-26 17:11:10 +0900

    refactor: JwtTokenResolver - 메소드 단일 책임으로 분리

--- dataportal-web/src/main/java/com/hanyang/dataportal/core/jwt/component/JwtTokenResolver.java
+++ dataportal-web/src/main/java/com/hanyang/dataportal/core/jwt/component/JwtTokenResolver.java
@@ -3,7 +3,6 @@ package com.hanyang.dataportal.core.jwt.component;
 import com.hanyang.dataportal.core.exception.JwtTokenException;
 import io.jsonwebtoken.Claims;
 import io.jsonwebtoken.Jwts;
-import jakarta.servlet.http.HttpServletRequest;
 import lombok.RequiredArgsConstructor;
 import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
 import org.springframework.security.core.Authentication;
@@ -12,7 +11,6 @@ import org.springframework.security.core.authority.SimpleGrantedAuthority;
 import org.springframework.security.core.userdetails.User;
 import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.stereotype.Component;
-import org.springframework.util.StringUtils;
 
 import java.util.Arrays;
 import java.util.Collection;
@@ -26,30 +24,53 @@ public class JwtTokenResolver {
 
     private final JwtSecretKey jwtSecretKey;
 
-    // JWT 토큰을 복호화하여 토큰에 들어있는 정보를 꺼내는 메서드
-    public Authentication getAuthentication(String accessToken) {
-        // 토큰 복호화
-        Claims claims = Jwts.parserBuilder().setSigningKey(jwtSecretKey.getKey()).build().parseClaimsJws(accessToken).getBody();
+    /**
+     * 액세스 토큰을 복호화하는 메서드
+     * @param accessToken
+     * @return
+     */
+    private Claims parseToken(String accessToken) {
+        return Jwts.parserBuilder()
+                .setSigningKey(jwtSecretKey.getKey())
+                .build()
+                .parseClaimsJws(accessToken)
+                .getBody();
+    }
+
+    /**
+     * 클레임에서 권한 정보를 꺼내오는 메서드
+     * @param claims
+     * @return
+     */
+    private Collection<? extends GrantedAuthority> getAuthorities(Claims claims) {
+        return Arrays.stream(claims.get("auth").toString().split(","))
+                .map(SimpleGrantedAuthority::new)
+                .collect(Collectors.toList());
+    }
+
+    /**
+     * 복호화한 액세스 토큰을 검증하는 메서드
+     * @param claims
+     */
+    private void validateClaims(Claims claims) {
         if (claims.get("auth") == null) {
             throw new JwtTokenException(INVALID_JWT);
         }
+    }
 
-        // 클레임에서 권한 정보 가져오기
-        Collection<? extends GrantedAuthority> authorities =
-                Arrays.stream(claims.get("auth").toString().split(","))
-                        .map(SimpleGrantedAuthority::new)
-                        .collect(Collectors.toList());
+    /**
+     * JWT 토큰을 복호화하여 토큰에 들어있는 정보를 꺼내는 메서드
+     * @param accessToken
+     * @return
+     */
+    public Authentication getAuthentication(String accessToken) {
+        final Claims claims = parseToken(accessToken);
+        validateClaims(claims);
+
+        final Collection<? extends GrantedAuthority> authorities = getAuthorities(claims);
 
         // UserDetails 객체를 만들어서 Authentication 리턴
         UserDetails principal = new User(claims.getSubject(), "", authorities);
         return new UsernamePasswordAuthenticationToken(principal, "", authorities);
-    }
-
-    public String extractToken(HttpServletRequest request) {
-        String bearerToken = request.getHeader("Authorization");
-        if (StringUtils.hasText(bearerToken) && bearerToken.startsWith("Bearer")) {
-            return bearerToken.substring(7);
-        }
-        return null;
     }
 }
