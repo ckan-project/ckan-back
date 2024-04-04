07222c572a7424bf40cf72bf496a043761909af0
장시영 <sysmile77@gmail.com>
2024-04-04 14:20:25 +0900

    Feat: Question Controller Test Method (save, update)

---
+++ dataportal-web/src/test/java/com/hanyang/dataportal/qna/controller/QuestionControllerTest.java
@@ -0,0 +1,2 @@
+package com.hanyang.dataportal.qna.controller;public class QuestionControllerTest {
+}
---
+++ dataportal-web/src/test/java/com/hanyang/dataportal/qna/service/AnswerServiceTest.java
@@ -0,0 +1,2 @@
+package com.hanyang.dataportal.qna.service;public class AnswerServiceTest {
+}
