302275c41ccff507de280a60bbb19d704fea3532
mintaek22 <biba99@naver.com>
2024-04-05 22:35:25 +0900

    fix:notice api url 변경

--- dataportal-web/src/main/java/com/hanyang/dataportal/notice/controller/NoticeController.java
+++ dataportal-web/src/main/java/com/hanyang/dataportal/notice/controller/NoticeController.java
@@ -6,6 +6,8 @@ import com.hanyang.dataportal.notice.dto.req.ReqNoticeDto;
 import com.hanyang.dataportal.notice.dto.res.ResNoticeDto;
 import com.hanyang.dataportal.notice.dto.res.ResNoticeListDto;
 import com.hanyang.dataportal.notice.service.NoticeService;
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.tags.Tag;
 import lombok.RequiredArgsConstructor;
 import org.springframework.data.domain.Page;
 import org.springframework.http.ResponseEntity;
@@ -13,41 +15,42 @@ import org.springframework.security.core.annotation.AuthenticationPrincipal;
 import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.web.bind.annotation.*;
 
+@Tag(name = "공지사항 API")
 @RestController
 @RequiredArgsConstructor
-@RequestMapping("/api/notices")
+@RequestMapping("/api")
 public class NoticeController {
     private final NoticeService noticeService;
 
-    // 1. 공지사항 작성
-    @PostMapping("/create")
+    @Operation(summary = "공지사항 작성")
+    @PostMapping("/notice")
     public ResponseEntity<ApiResponse<ResNoticeDto>> createNotice(@AuthenticationPrincipal UserDetails userDetails, @RequestBody ReqNoticeDto reqNoticeDto ) {
         Notice notice = noticeService.create(reqNoticeDto, userDetails.getUsername());
         return ResponseEntity.ok(ApiResponse.ok(new ResNoticeDto(notice)));
     }
 
-    // 2-1. 공지사항 조회 (단건조회)
-    @GetMapping("/{noticeId}")
+    @Operation(summary = "공지사항 상세보기")
+    @GetMapping("/notice/{noticeId}")
     public ResponseEntity<ApiResponse<ResNoticeDto>> findNotice(@PathVariable Long noticeId) {
         Notice notice = noticeService.getNoticeDetail(noticeId);
         return ResponseEntity.ok(ApiResponse.ok(new ResNoticeDto(notice)));
     }
 
-    // 2-2. 공지사항 조회 (리스트 조회)
-    @GetMapping("/list")
+    @Operation(summary = "공지사항 리스트 보기")
+    @GetMapping("/notices")
     public ResponseEntity<ApiResponse<ResNoticeListDto>> findListNotice(@RequestParam Integer page) {
         Page<Notice> noticeList = noticeService.getNoticeList(page);
         return ResponseEntity.ok(ApiResponse.ok(new ResNoticeListDto(noticeList)));
     }
 
-    // 3. 공지사항 수정 (update)
-    @PostMapping("/update/{noticeId}")
+    @Operation(summary = "공지사항 수정")
+    @PostMapping("/notice/{noticeId}")
     public ResponseEntity<ApiResponse<ResNoticeDto>> updateNotice(@PathVariable Long noticeId, @RequestBody ReqNoticeDto reqNoticeDto) {
         Notice notice = noticeService.update(reqNoticeDto, noticeId);
         return ResponseEntity.ok(ApiResponse.ok(new ResNoticeDto(notice)));
     }
-    // 4. 공지사항 삭제
-    @DeleteMapping("/delete/{noticeId}")
+    @Operation(summary = "공지사항 삭제")
+    @DeleteMapping("/notice/{noticeId}")
     public ResponseEntity<ApiResponse<?>> deleteNotice(@PathVariable Long noticeId) {
         noticeService.delete(noticeId);
         return ResponseEntity.ok(ApiResponse.ok(null));
