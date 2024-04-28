6000017a4446ea6d2613dc0604e6f31f7e9f0ba1
mintaek22 <biba99@naver.com>
2024-04-28 16:06:18 +0900

    fix:swagger cors 오류 해결

---
+++ dataportal-datastore/src/main/java/com/hanyang/datastore/core/config/CorsConfig.java
@@ -0,0 +1,27 @@
+package com.hanyang.datastore.core.config;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.cors.CorsConfiguration;
+import org.springframework.web.cors.CorsConfigurationSource;
+import org.springframework.web.cors.UrlBasedCorsConfigurationSource;
+
+import java.util.List;
+
+@Configuration
+public class CorsConfig {
+
+    @Bean
+    public CorsConfigurationSource corsConfigurationSource() {
+        CorsConfiguration config = new CorsConfiguration();
+        config.setAllowCredentials(true);
+        config.addAllowedOrigin("https://hy-data.site:8081/");
+        config.addAllowedOrigin("http://localhost:3000/");
+        config.setAllowedMethods(List.of("GET", "POST", "DELETE", "PUT"));
+        config.setAllowedHeaders(List.of("*"));
+
+        UrlBasedCorsConfigurationSource source = new UrlBasedCorsConfigurationSource();
+        source.registerCorsConfiguration("/**", config);
+        return source;
+    }
+}
