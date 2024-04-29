accda6b06605cc7d55df4b56b795bcae892263a3
minSsan <msp1125@hanyang.ac.kr>
2024-04-29 18:30:09 +0900

    fix: ReqOauthDto 객체 400 에러

--- dataportal-web/src/main/java/com/hanyang/dataportal/user/dto/req/ReqOauthDto.java
+++ dataportal-web/src/main/java/com/hanyang/dataportal/user/dto/req/ReqOauthDto.java
@@ -2,9 +2,11 @@ package com.hanyang.dataportal.user.dto.req;
 
 import lombok.AllArgsConstructor;
 import lombok.Data;
+import lombok.NoArgsConstructor;
 
 @Data
 @AllArgsConstructor
+@NoArgsConstructor
 public class ReqOauthDto {
     private String code;
 }
