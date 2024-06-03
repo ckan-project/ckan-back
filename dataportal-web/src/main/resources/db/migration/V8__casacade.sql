3384dc77d64d32b00046738cf8341a20d2b733bc
mintaek22 <biba99@naver.com>
2024-06-03 12:25:17 +0900

    fix: dataset cascade 이름 중복 제거

--- dataportal-web/src/main/resources/db/migration/V8__casacade.sql
+++ dataportal-web/src/main/resources/db/migration/V8__casacade.sql
@@ -1,11 +1,11 @@
-alter table hanyang.resource drop foreign key resource_ibfk_1;
+alter table hanyang.resource drop foreign key fk_dataset;
 
 alter table hanyang.resource
 add constraint resource_ibfk_1
 foreign key (dataset_id) references hanyang.dataset(dataset_id)
 on delete cascade;
 
-alter table hanyang.scrap drop foreign key scrap_ibfk_1;
+alter table hanyang.scrap drop foreign key scrap_ibfk_2;
 
 alter table hanyang.scrap
 add constraint scrap_ibfk_1
@@ -13,6 +13,7 @@ foreign key (dataset_id) references hanyang.dataset(dataset_id)
 on delete cascade;
 
 alter table hanyang.download drop foreign key download_ibfk_1;
+alter table hanyang.download drop foreign key download_ibfk_2;
 
 alter table hanyang.download
 add constraint download_ibfk_1
